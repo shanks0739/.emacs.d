315c0d79d7d8015970b3a0f87c8502f3f12860c7
shanks0739 <476241180@qq.com>
2015-12-31 17:10:01 +0800

    smex

--- package/env_development.el
+++ package/env_development.el
@@ -15,14 +15,38 @@
 (yas-minor-mode-on) ; 以minor mode打开，这样才能配合主mode使用
 
 ;;#####################################################################
+;; M-x help
+(require 'smex)
+(smex-initialize) 
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
+;
+;(global-set-key [(meta x)] (lambda ()
+;			     (interactive)
+;			     (or (boundp 'smex-cache)
+;				 (smex-initialize))
+;			     (global-set-key [(meta x)] 'smex)
+;			     (smex)))
+;
+;(global-set-key [(shift meta x)] (lambda ()
+;				   (interactive)
+;				   (or (boundp 'smex-cache)
+;				       (smex-initialize))
+;				   (global-set-key [(shift meta x)] 'smex-major-mode-commands)
+;				                                      (smex-major-mode-commands)))
+;
+;
+;(autoload 'smex "smex"
+;    "Smex is a M-x enhancement for Emacs, it provides a convenient interface to your recently and most frequently used commands.")
+;
+;(global-set-key (kbd "M-x") 'smex)
+;
 
-;(require 'smex)
-
-
- 
 ;;#####################################################################
-;; cedet
+;; cedet 代码补全工具，也可以是代码浏览，引用等功能
 ;(require 'cedet)
 ;(semantic-load-enable-excessive-code-helpers)
 ;(global-ede-mode t)
@@ -31,7 +55,7 @@
 ;(global-srecode-minor-mode 1)
 ;(semantic-load-enable-semantic-debugging-helpers)
 
-;; ecb
+;; ecb 这个工具使emacs 看起来像ide
 ;(require 'ecb)
 ;(setq stack-trace-on-error t)
 ;(ecb-activate)
