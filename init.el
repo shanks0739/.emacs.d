08c7e2d0fe32e381b469bfe8d516b6e93b56650c
shanks0739 <476241180@qq.com>
2016-01-08 17:50:05 +0800

    m

--- init.el
+++ init.el
@@ -21,13 +21,14 @@
       (normal-top-level-add-subdirs-to-load-path)
       (nconc load-path orig-load-path))
 
-;; basic conf
-(load "basic_conf")
 
 ;; 开发环境配置，如自动补全，符号定位和引用，颜色配置
 (load "env_development")
 
 (load "tags_conf")
+
+;; basic conf
+(load "basic_conf")
 
 ;(require 'color-theme)
 ;(setq color-theme-is-global t)
@@ -35,17 +36,3 @@
 ;(color-theme-matrix)
 ;(color-theme-gnome2)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ecb-options-version "2.40")
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
